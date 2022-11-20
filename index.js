487f6ab61e57773556bbe38b12a100574d0d71a4
obaidul islam1 <mursalinislam537215@gmail.com>
2022-11-20 17:24:47 +0600

    server primary set up complete

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const express = require('express')
 const cors = require('cors')
 const port = process.env.PORT || 5000;
+
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req,res) =>{
+    res.send('assignment 11 running')
+})
+
+app.listen(port,() =>{
+  console.log(`server is running port ${port}`);  
+})
