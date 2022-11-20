b76f518cade7198849ebbf14327a25289ad43683
obaidul islam1 <mursalinislam537215@gmail.com>
2022-11-20 17:52:35 +0600

    mongodb added complete

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ub5swj4.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 app.get('/', (req,res) =>{
     res.send('assignment 11 running')
