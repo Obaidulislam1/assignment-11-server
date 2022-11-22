759a463d072ba2f543643f611ad5ab6885792416
obaidul islam1 <mursalinislam537215@gmail.com>
2022-11-22 23:18:56 +0600

    update user start

--- index.js
+++ index.js
@@ -52,9 +52,15 @@ async function run() {
         })
         app.delete('/review/:id', async(req,res) =>{
           const id = req.params.id;
-          const query =  {_id: ObjectId(id)}
+          const query =  {_id: ObjectId(id)};
           const result = await userReview.deleteOne(query);
           res.send(result)
+        })
+        app.get('/review/:id', async(req,res) =>{
+             const id = req.params.id;
+             const query = {_id : ObjectId(id)}
+             const result = await userReview.findOne(query)
+             res.send(result)
         })
         app.get('/review', async(req,res) =>{
             let query = {}
